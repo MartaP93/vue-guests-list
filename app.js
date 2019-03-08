5d892dbdd53dfe32980a76dde90978736773b661
Marta Puszyńska <marta.puszynska93@gmail.com>
2019-03-08 21:47:33 +0100

    guests-lists-outputting-text-and-html

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ new Vue({
         event: {
             eventDate: 'August 14th - 16th',
             eventTitle: 'Summer Festival!',
+            eventTitle2: 'v text output',
+            signUpText: 'Add your name to the guest list for <em>exclusive</em> offers:',
             eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
         },
         newNameText: '',
         guestName: [],
-        formSubmitClass: ""
+        formSubmitClass: "",
+        appStyles: {
+            marginTop: '25px'
+        }
     },
     methods: {
         formSubmitted: function () {
