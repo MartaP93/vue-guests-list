f705d2bf8d3656e15a4368ff0da2725fd0ce5a51
Marta Puszyńska <marta.puszynska93@gmail.com>
2019-03-08 20:31:27 +0100

    guests-list-name-adding-for-loop

---
+++ app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    data: {
+        event: {
+            eventDate: 'August 14th - 16th',
+            eventTitle: 'Summer Festival!',
+            eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+        },
+        newNameText: '',
+        guestName: []
+    },
+    methods: {
+        formSubmitted: function () {
+            this.guestName.push(this.newNameText)
+            this.newNameText = ''
+        }
+    },
+});
